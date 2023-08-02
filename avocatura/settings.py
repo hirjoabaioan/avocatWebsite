7fa4640fbaf99f975838bfcab29c6e14f4d5c902
Ioan Hîrjoabă <hirjoaba.ioan@gmail.com>
2023-08-03 00:01:08 +0300

    Update settings.py

--- avocatura/settings.py
+++ avocatura/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['avocaturaweb.herokuapp.com']
+# ALLOWED_HOSTS = ['avocaturaweb.herokuapp.com']
+ALLOWED_HOSTS = ['avocatwebsite-536ffabbed2c.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
